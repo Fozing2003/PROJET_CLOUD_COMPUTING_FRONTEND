8dba68cfa72b09a3418ea2b922024e4704abf950
lisefozing <fozinglise@gmail.com>
2025-12-20 13:44:01 +0100

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,19 @@
-module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/*/.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
         green: {
-          600: '#16a34a', // Vibrant green for text-green-600
-          700: '#15803d', // Darker green for hover states
-          800: '#166534', // Dark green for backgrounds
+          600: '#16a34a',
+          700: '#15803d',
+          800: '#166534',
         },
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
