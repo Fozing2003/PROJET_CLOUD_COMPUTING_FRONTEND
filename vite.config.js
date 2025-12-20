c1d938686bdcedd6b0cbf73e3fd650d0463882d6
IVANS KOUAKEP <kouakepivan@gmail.com>
2025-12-20 21:26:06 +0100

    Tailwind

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
     host: '0.0.0.0',
     port: process.env.PORT || 10000,
   },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 10000,
+    allowedHosts: [
+      'projet-cloud-computing.onrender.com',
+      'projet-cloud-computing-frontend-2.onrender.com',
+    ],
+  },
 })
