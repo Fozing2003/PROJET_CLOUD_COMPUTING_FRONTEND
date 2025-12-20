0ae1b970f02d0a63f39d697b7d301fd32f71fc5f
lisefozing <fozinglise@gmail.com>
2025-12-20 13:11:09 +0100

    modification d'un fichier

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 10000,
-
-     allowedHosts: [
-     'projet-cloud-computing-frontend-2.onrender.com',
-     ],
-
-  },
+  base: '/',   // IMPORTANT pour les CSS
 })
