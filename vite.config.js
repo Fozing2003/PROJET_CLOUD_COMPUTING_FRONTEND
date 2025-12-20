aca361742a767d812d5717ef39f7f2b5eed96c4f
IVANS KOUAKEP <kouakepivan@gmail.com>
2025-12-20 21:07:12 +0100

    Tailwind et vite_config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
+  base: './',
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 10000,
-    allowedHosts: [
-      'projet-cloud-computing-frontend-2.onrender.com',
-    ],
   },
 })
