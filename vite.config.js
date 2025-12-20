21946395197d182a6010118ba57e599c41b99af4
IVANS KOUAKEP <kouakepivan@gmail.com>
2025-12-20 20:02:39 +0100

    Fichier_Config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',   // IMPORTANT pour les CSS
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 10000,
+    allowedHosts: [
+      'projet-cloud-computing-frontend-2.onrender.com',
+    ],
+  },
 })
